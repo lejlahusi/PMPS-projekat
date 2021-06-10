d43c6f5c64cd22c5dd05833af069130757d760c6
lejlahusi <80544994+lejlahusi@users.noreply.github.com>
2021-06-10 19:47:27 +0200

    Create main.h

---
+++ main.h
@@ -0,0 +1,114 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define Blue_Button_Pin GPIO_PIN_0
+#define Blue_Button_GPIO_Port GPIOA
+#define Blue_Button_EXTI_IRQn EXTI0_IRQn
+#define LED_Pin GPIO_PIN_5
+#define LED_GPIO_Port GPIOA
+#define LED1_Pin GPIO_PIN_12
+#define LED1_GPIO_Port GPIOD
+#define LED2_Pin GPIO_PIN_13
+#define LED2_GPIO_Port GPIOD
+#define LED3_Pin GPIO_PIN_14
+#define LED3_GPIO_Port GPIOD
+#define LED4_Pin GPIO_PIN_15
+#define LED4_GPIO_Port GPIOD
+#define TRIG_Pin GPIO_PIN_8
+#define TRIG_GPIO_Port GPIOA
+#define ECHO_Pin GPIO_PIN_9
+#define ECHO_GPIO_Port GPIOA
+#define D0_Pin GPIO_PIN_0
+#define D0_GPIO_Port GPIOD
+#define D1_Pin GPIO_PIN_1
+#define D1_GPIO_Port GPIOD
+#define D2_Pin GPIO_PIN_2
+#define D2_GPIO_Port GPIOD
+#define D3_Pin GPIO_PIN_3
+#define D3_GPIO_Port GPIOD
+#define D4_Pin GPIO_PIN_4
+#define D4_GPIO_Port GPIOD
+#define D5_Pin GPIO_PIN_5
+#define D5_GPIO_Port GPIOD
+#define D6_Pin GPIO_PIN_6
+#define D6_GPIO_Port GPIOD
+#define D7_Pin GPIO_PIN_7
+#define D7_GPIO_Port GPIOD
+#define RS_Pin GPIO_PIN_3
+#define RS_GPIO_Port GPIOB
+#define E_Pin GPIO_PIN_4
+#define E_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+/*extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim4;
+extern UART_HandleTypeDef huart2;*/
+/* USER CODE BEGIN PFP */
+	
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
